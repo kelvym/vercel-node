04aaa7424e45d9a5b0d125d440d5e74a4d594a01
Kelvym Miranda <kelvymm@gmail.com>
2023-04-13 01:46:55 -0300

    init

---
+++ api/test.js
@@ -0,0 +1,8 @@
+import { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default function handler(req, res) {
+  const { name = 'World' } = req.query
+  return res.json({
+    message: `Hello ${name}!`,
+  })
+}
